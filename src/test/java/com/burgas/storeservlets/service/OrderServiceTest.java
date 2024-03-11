5223c9f36818ae4ea96f9b7f3036198ca0dce53d
Burgas <burgassme@gmail.com>
2024-03-12 00:24:57 +0700

    Добавление тестов

--- src/test/java/com/burgas/storeservlets/service/OrderServiceTest.java
+++ src/test/java/com/burgas/storeservlets/service/OrderServiceTest.java
@@ -1,10 +1,13 @@
 package com.burgas.storeservlets.service;
 
 
+import com.burgas.storeservlets.entity.Order;
 import com.burgas.storeservlets.entity.OrderProduct;
+import com.burgas.storeservlets.entity.Product;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderServiceTest {
@@ -15,12 +18,23 @@ public class OrderServiceTest {
         OrderService orderService = new OrderService();
         String orderNumber = "A25";
         List<OrderProduct> info = orderService.getInfo(orderNumber);
+
         Assertions.assertEquals(2, info.size());
-        Assertions.assertEquals("Apples", info.get(0).getProduct().getName());
-        Assertions.assertEquals("Potatoes", info.get(1).getProduct().getName());
-        Assertions.assertEquals(150.5, info.get(0).getProduct().getPrice());
-        Assertions.assertEquals(75.34, info.get(1).getProduct().getPrice());
-        Assertions.assertEquals(5, info.get(0).getProductCount());
-        Assertions.assertEquals(12, info.get(1).getProductCount());
+        Assertions.assertIterableEquals(
+                info,
+                new ArrayList<>(
+                        List.of(
+                                new OrderProduct(new Product("Apples","For eat",150.5),
+                                        new Order(orderNumber,"2024-03-11 16:46:40.333745"),
+                                        5
+                                ),
+                                new OrderProduct(
+                                        new Product("Potatoes","For eat",75.34),
+                                        new Order(orderNumber,"2024-03-11 16:46:40.333745"),
+                                        12
+                                )
+                        )
+                )
+        );
     }
 }
